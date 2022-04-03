0469fa1eda336aea653cbf03de1a42c2d8fc14a6
CMC06 <crzychelc@gmail.com>
2022-04-03 14:48:26 -0400

    final commit predeploy

---
+++ dal.js
@@ -0,0 +1,74 @@
+const { MongoClient } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.4siiz.mongodb.net/bad-bank?retryWrites=true&w=majority`;
+//client to connect to for data
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+//collection where bad-bank data is housed
+const collection = client.db("bad-bank").collection('users');
+
+//create user account
+const create = (name, email, password) => {
+  return new Promise((resolve, reject) => {
+    client.connect(err => {
+      const doc = {name, email, password, balance: 0};
+      collection.insertOne(doc, {w:1}, (err, result) => {
+        err ? reject(err) : resolve(doc);
+      });
+    });
+    
+  });
+}
+
+//get all user accounts
+const allUsers = () => {
+  return new Promise((resolve, reject) => {
+    client.connect(err => {
+      collection.find({})
+      .toArray((err, docs) => {
+        err ? reject(err) : resolve(docs);
+      });
+    });
+  });
+}
+
+//update balance info
+const updateBalance = (email, newBalance) => {
+  let balance = Number(newBalance)
+  return new Promise((resolve, reject) => {
+    client.connect(err => {
+      collection.updateOne(
+        {"email": email}, 
+        {
+          $set: {"balance": balance}
+        })
+        .then(doc => {
+          resolve(doc);
+        })
+        .catch(err => {
+          reject(err);
+        })
+        if(err){
+          console.log(err);
+        }
+      });
+  });
+}
+
+//find one user, should return null if no existing user--if user exists, it will return user document
+const checkForAccount = (email) => {
+  return new Promise ((resolve, reject) => {
+    client.connect(err => {
+      collection.findOne(
+        { email: email }
+      )
+      .then((user) => {
+        resolve(user);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+    });
+  });
+}
+
+
+module.exports = { create, allUsers, updateBalance, checkForAccount };
---
+++ index.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const app = express();
+require('dotenv').config()
+const cors = require('cors');
+const dal = require('./dal.js');
+
+//allow cross origin access
+app.use(cors());
+app.use(express.static('build'));
+
+//create user account
+app.get('/account/create/:name/:email/:password', (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  const email = req.params.email;
+  dal.checkForAccount(email)
+    .then((user) => {
+      if(user === null){
+        dal.create(req.params.name, email, req.params.password)
+          .then((user) => {
+            console.log(user);
+            res.send(user);
+          })
+          .catch((err) => {
+            console.log(`Error: ${err}`);
+          });
+      } else {
+        res.status(409).send(`Cannot create new user, as ${req.params.email} is already attached to an existing account. Please log into your account or sign up for a new account with a unique email address.`)
+      }
+    })
+    .catch((err) => {
+      console.log(`Error: ${err}`);
+    })
+});
+
+// login user
+app.get('/account/login/:email/:password', (req, res) => {
+  const email = req.params.email;
+  dal.checkForAccount(email)
+    .then((user) => {
+      if(user !== null){
+        if(req.params.password === user.password){
+          res.send(user);
+        } else {
+          res.status(401).send(`Either user email or user password do not match our database. If you have not yet created an account, please do so by clicking 'Create An Account'`);
+        }
+      } else {
+        res.status(409).send(`Either user email or user password do not match our database. If you have not yet created an account, please do so by clicking 'Create An Account'`);
+      }
+    })
+    .catch((err) => {
+      console.log(`Error: ${err}`);
+    })
+});
+
+//all accounts
+app.get('/account/all', (req, res) => {
+  dal.allUsers().
+    then((docs) => {
+      console.log(docs);
+      res.send(docs);
+    })
+    .catch((err) => {
+      console.log(`Error: ${err}`);
+    });
+});
+
+//update balance info
+app.get('/account/updateBalance/:email/:balance', (req, res) => {
+  const email = req.params.email;
+  dal.updateBalance(email, req.params.balance)
+    .then((doc) => {
+      if(doc.modifiedCount === 1){
+        dal.checkForAccount(req.params.email)
+          .then(user => res.send(user))
+          .catch(err => console.log(err))
+      } 
+    })
+    .catch((err) => console.log(`Balance updated rejected due to error: ${err}`))
+});
+
+//check user
+app.get('/account/checkUser/:email', (req, res) => {
+  const email = req.params.email;
+  dal.checkForAccount(email)
+    .then(user => {
+      res.send(user);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+});
+
+//reroute any other routes back to root on refresh
+app.get('*', (req, res) => {
+  res.redirect(302, '/')
+});
+
+const port = 5000;
+app.listen(port);
+console.log(`Running on port: ${port}`);
