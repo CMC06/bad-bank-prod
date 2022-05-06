211f0a98c1d0e65f13bb1bbc2b98d3f8ade5f65e
CMC06 <crzychelc@gmail.com>
2022-05-06 16:07:12 -0400

    updating card headlines for UI, updated build folder for serving

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
-require('dotenv').config()
 const cors = require('cors');
 const dal = require('./dal.js');
 
